8c39e2d55b5a08b45a4c500765587db70fd06de8
Kevin Mark <kevinmark281@hotmail.com>
2022-01-28 14:02:38 -0800

    try to trigger build

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,9 @@
 
 # app/controllers/articles_controller.rb
 class ArticlesController < ApplicationController
+  
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
-include Visisble
+include Visible
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true
